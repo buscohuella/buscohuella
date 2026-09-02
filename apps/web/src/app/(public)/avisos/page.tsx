8e43be5f4be09671bc0574f6d4fdc8cbf1f015db
xavikuDEV <xaviku91@gmail.com>
2026-09-02 18:28:16 +0200

    fix(navigation):redirect-legacy-notice-list

--- apps/web/src/app/(public)/avisos/page.tsx
+++ apps/web/src/app/(public)/avisos/page.tsx
@@ -1,23 +1,5 @@
-import { Breadcrumbs } from '@/components/layout/breadcrumbs';
-import { getServerTranslator } from '@/features/i18n/server';
-import { PublicReportsPage } from '../reportes/page';
+import { redirect } from 'next/navigation';
 
-export default async function PublicNoticesPage() {
-  const { translate } = await getServerTranslator();
-
-  return (
-    <>
-      <div className="mx-auto max-w-7xl px-4 pt-6 sm:px-6 lg:px-8">
-        <Breadcrumbs
-          label={translate('noticesHub.breadcrumbs.label')}
-          items={[
-            { href: '/', label: translate('noticesHub.breadcrumbs.home') },
-            { href: '/mis-avisos', label: translate('noticesHub.breadcrumbs.section') },
-            { label: translate('noticesHub.breadcrumbs.explore') },
-          ]}
-        />
-      </div>
-      <PublicReportsPage />
-    </>
-  );
+export default function PublicNoticesPage() {
+  redirect('/explorar-avisos');
 }
